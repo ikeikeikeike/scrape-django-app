2c1e800e7f98f0ad1cba9c0731c9710acb71992c
ikeikeikeike <jp.ne.co.jp@gmail.com>
2016-06-22 04:04:07 +0900

    app

---
+++ scrape/feeder/management/commands/feed.py
@@ -0,0 +1,23 @@
+from django.core.management.base import BaseCommand  # , CommandError
+
+from core.scraper import feed
+
+
+class Command(BaseCommand):
+    # def add_arguments(self, parser):
+    #     # Positional arguments
+    #     parser.add_argument('poll_id', nargs='+', type=int)
+
+    #     # Named (optional) arguments
+    #     parser.add_argument(
+    #         '--delete',
+    #         action='store_true',
+    #         dest='delete',
+    #         default=False,
+    #         help='Delete poll instead of closing it',
+    #     )
+        #  if options['delete']:
+
+    def handle(self, *args, **options):
+        scrape = feed.Scrape()
+        print(scrape.description())
