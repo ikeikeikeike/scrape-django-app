7676780bc562de8fbb3ce0602d7f7e32a7d03dc0
ikeikeikeike <jp.ne.co.jp@gmail.com>
2016-07-11 17:28:59 +0900

    fill up into commands

--- scrape/diva/management/commands/divaorg18.py
+++ scrape/diva/management/commands/divaorg18.py
@@ -1,28 +1,10 @@
 from django.core.management.base import BaseCommand
 
-from core.extractors import diva
-from core.extractor import (
-    safe_kana,
-    safe_romaji,
-    separate_alias
-)
-
-from core import models
+from core import query
+from core.extractors import org18
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        person = diva.Actresses()
-        objs = models.Diva.objects
-
-        for _, people in person.all().items():
-            for p in people:
-                name, alias = separate_alias(p['name'])
-                alias = ','.join(alias)
-
-                d, created = objs.get_or_create(name=name, alias=alias)
-                if created:
-                    d.kana = safe_kana(p['yomi'])
-                    d.romaji = safe_romaji(p['oto'])
-                    d.gyou = p['gyou']
-                    d.save()
+        for _, p in org18.Diva().json:
+            query.diva_upsert_by_org18(p)
