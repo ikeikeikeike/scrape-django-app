7dfdef3845afcdd10aa7b047300a6f580ea55c4a
ikeikeikeike <jp.ne.co.jp@gmail.com>
2017-01-04 23:36:14 +0900

    scheduler.

--- schedule/extoon.py
+++ schedule/extoon.py
@@ -23,9 +23,9 @@ cron = Plan(
 #  cron.module('calendar', every='feburary', at='day.3')
 
 cron.command('cd %s && DJANGO_SETTINGS_MODULE=scrape.settings_production $HOME/venv/bin/scrapy crawl eoaient' % (pjoin(dir_path, '../scrape/crawler')),
-    every='1.day', at='minute.48')
+    every='2.day', at='minute.48')
 cron.command('cd %s && DJANGO_SETTINGS_MODULE=scrape.settings_production $HOME/venv/bin/scrapy crawl ck0tp' % (pjoin(dir_path, '../scrape/crawler')),
-    every='1.day', at='minute.12')
+    every='3.day', at='minute.12')
 
 cron.script('manage.py extoon_info', every='5.hour', at='minute.30')
 cron.script('manage.py extoon_description', every='6.hour', at='minute.15')
